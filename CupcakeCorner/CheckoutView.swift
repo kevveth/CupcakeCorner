3efa571d4c531f4cf26b079d860b8d76fc4fef15
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-04-17 01:11:00 -0700

    Address validation and checkout view

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Kenneth Oliver Rathbun on 4/17/24.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
