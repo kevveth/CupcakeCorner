a92934ea1c2424677b84c96c727d4c275a981a8c
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-04-17 01:11:56 -0700

    Address validation and check out view

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -24,4 +24,33 @@ class Order {
     }
     var extraFrosting = false
     var addSprinkles = false
+    
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        
+        return true
+    }
+    
+    var cost: Double {
+        // 2$ per cake
+        var cost = Double(quantity) * 2
+        
+        // complicated cakes cost more
+        cost += (Double(type) / 2)
+        
+        // 1$/cake for extra frosting
+        cost += Double(quantity) * 1
+        
+        // 0.50$/cake for sprinkles
+        cost += Double(quantity) / 2
+        
+        return cost
+    }
 }
